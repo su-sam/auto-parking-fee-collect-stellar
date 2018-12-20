096104d7cc60a9fa3d79c8547b91e481aa1b696f
intern <nana15492@gmail.com>
2018-12-20 18:20:51 +0700

    update

--- lab/test.js
+++ lab/test.js
@@ -1,86 +1,110 @@
-const StellarSdk = require('stellar-sdk')
-StellarSdk.Network.useTestNetwork()
-const server = new StellarSdk.Server('https://horizon-testnet.stellar.org')
+const StellarSdk = require('stellar-sdk');
+StellarSdk.Network.useTestNetwork();
+const server = new StellarSdk.Server('https://horizon-testnet.stellar.org');
 
 // ===============================A=P=P===============================
 // params @BLEAccID from challenge-response
-const sourceKey = 'GC3VDJXZKI6JK6Q5LL5AHS6LXVDQG5T6LDGLYJADDIDYXLN2EPUYRBKM';
+const blePK = 'GC3VDJXZKI6JK6Q5LL5AHS6LXVDQG5T6LDGLYJADDIDYXLN2EPUYRBKM';
 // App Store his data
 const appKeys = StellarSdk.Keypair.fromSecret('SBSFFFZP53H37E7RFWSVPRG3JE3BMQT25HLIOQOBCTPY5RKJVG6GFQ4H');
-const license = '1กง6798';
-// create tx
-async function createTx(sourceKey, license) {
-    const source = await server.loadAccount(sourceKey);
-    if(!source) return console.log('Invalid Source Account ID');
+const vehicleNo = '1กง6798';
 
+// FUNCTION to create transaction envelope
+async function createEnvelope(blePK, vehicleNo) {
+    // isValid Account
+    const bleAcc = await server.loadAccount(blePK);
+    if(!bleAcc) return console.log('Invalid BLE Account ID', blePK);
     const appAcc = await server.loadAccount(appKeys.publicKey());
-    if(!appAcc) return console.log('Invalid Application Account ID');
+    if(!appAcc) return console.log('Invalid Account ID');
+    // check is App have native
+    const appAccBalanceNaive = appAcc.balances.find(item => item.asset_type === 'native')
+    if (!appAccBalanceNaive) {
+        throw new Error('App Account does not have native balance')
+    }
+    // check is native >= 1
+    const nativeBalance = Number(appAccBalanceNaive.balance)
+    const MAX_BALANCE_ALLOWANCE = 1
+    if (nativeBalance < MAX_BALANCE_ALLOWANCE) {
+        throw new Error(`App Account must have balance > ${MAX_BALANCE_ALLOWANCE}`)
+    }
 
-    // create transaction envelope
-    const tx = new StellarSdk.TransactionBuilder(source)
+    // create new transaction envelope
+    const tx = new StellarSdk.TransactionBuilder(bleAcc)
     .addOperation(StellarSdk.Operation.manageData({
         name : appKeys.publicKey(),
-        value : license
+        value : vehicleNo
     }))
-    //.addMemo(StellarSdk.Memo.text(license))
+    //.addMemo(StellarSdk.Memo.text('entry'))
     .build()
 
+    // create envelope
     const envelope = await tx.toEnvelope();
-    //console.log(envelope)
+
     return envelope;
-    // envelope._attributes.tx._attributes.operations[0]._attributes.body._value._attributes.dataName
-    // envelope._attributes.tx._attributes.operations[0]._attributes.body._value._attributes.dataValue
 }
 
 // ===============================B=L=E===============================
 // params from camera
-const camLicense = '1กง6798';
+const camVehicleNo = '1กง6798';
 // BLE Store his data
 const bleKeys = StellarSdk.Keypair.fromSecret('SCMWGMAIPDRXWYDMSFXSOJO5NQ2DXSSTQYAT3FQ67M2GSRMCNAYUHU5Y');
 
-async function start() {
-    // for test
-    const envelope = await createTx(appKeys.publicKey(), license);
-        // console.log(envelope);
+// FUNCTION to sign transaction
+const signTransaction = async (envelope) => {
     const dataName = envelope._attributes.tx._attributes.operations[0]._attributes.body._value._attributes.dataName;
     const dataValue = envelope._attributes.tx._attributes.operations[0]._attributes.body._value._attributes.dataValue;
-        // console.log(dataName);
-        // console.log(dataValue);
-    if((compare(camLicense, dataValue))==0 ) //console.log(envelope.toXDR('base64')) //console.log('true')
-        await signTx(envelope, dataName);
-    else console.log('Wrong License from application')
+    // is there vehicle no. correct?
+    if((compare(camVehicleNo, dataValue)) === 0 ) await signTx(envelope, dataName); // then sign the tx
+    else console.log('Vehicle number from application is not correct');
 }
 
-function compare(camLicense, txLicense) {
-    const buf1 = Buffer.from(camLicense);
-    // const buf2 = Buffer.from(txLicense);
-    return(buf1.compare(txLicense));
+// FUNCTION for compare camera's and app's
+function compare(camVehicleNo, txVehicleNo) {
+    const buf = Buffer.from(camVehicleNo);
+    // txVehicle is already be a Buffer
+    return(buf.compare(txVehicleNo));
 }
 
+// FUNCTION for import envelope -> sign the envelope w/t bleSK
 async function signTx(envelope, appAccId) {
-    const source = await server.loadAccount(bleKeys.publicKey());
-    if(!source) return console.log('Invalid Source Account ID');
+    // isValid Account
+    const bleAcc = await server.loadAccount(blePK);
+    if(!bleAcc) return console.log('Invalid BLE Account ID', blePK);
+    const appAcc = await server.loadAccount(appKeys.publicKey());
+    if(!appAcc) return console.log('Invalid Account ID');
+    // check is App have native
+    const appAccBalanceNaive = appAcc.balances.find(item => item.asset_type === 'native')
+    if (!appAccBalanceNaive) {
+        throw new Error('App Account does not have native balance')
+    }
+    // check is native >= 1
+    const nativeBalance = Number(appAccBalanceNaive.balance)
+    const MAX_BALANCE_ALLOWANCE = 1
+    if (nativeBalance < MAX_BALANCE_ALLOWANCE) {
+        throw new Error(`App Account must have balance > ${MAX_BALANCE_ALLOWANCE}`)
+    }
 
-    const appAcc = await server.loadAccount(appAccId);
-    if(!appAcc) return console.log('Invalid Application Account ID');
-
-    console.log(envelope);
+    // create new transaction
     const tx = new StellarSdk.Transaction(envelope)
-    // // import the XDR 
-    // const tx = new StellarSdk.Transaction(encodeEnvelope);
-    // // sign transaction
-    tx.sign(bleKeys);
-    // // submit transaction
 
+    // sign envelope
+    tx.sign(bleKeys);
+
+    // submit transaction
     try {
         await server.submitTransaction(tx);
     } catch(e) {
         console.log('error when sumit tx: ', e)
     }
-    
-
-    // console.log(logs);
 }
 
+// ==============================T=E=S=T===============================
+async function start() {
+    // App create envelope
+    const envelope = await createEnvelope(blePK, vehicleNo);
+    // BLE sign transaction
+    await signTransaction(envelope);
+}
 start();
+
 
